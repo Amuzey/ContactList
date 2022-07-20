2ecbb7974f626ea3d5d193f5d33494efbc4d79f8
Amuzey <ivanovad97@gmail.com>
2022-07-20 23:01:27 +0500

    work with logic

--- ContactList/VievControllers/SecondContactListViewController.swift
+++ ContactList/VievControllers/SecondContactListViewController.swift
@@ -10,30 +10,37 @@ import UIKit
 class SecondContactListViewController: UITableViewController {
     
     private let contactsList = Person.getContactInfo()
-
+    
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
         contactsList.count
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      2
+        2
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "secondContact", for: indexPath)
-        let contact = contactsList[indexPath.row]
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "phone", for: indexPath)
+        let contact = contactsList[indexPath.section]
         var content = cell.defaultContentConfiguration()
-        content.text = contact.phoneNumber
+        
+        if indexPath.row == 0 {
+            content.text = contact.phoneNumber
+            content.image = UIImage(systemName: "phone")
+        } else if indexPath.row == 1 {
+            content.text = contact.email
+            content.image = UIImage(systemName: "tray")
+        }
+        
         cell.contentConfiguration = content
         return cell
     }
     
+    
+    
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        let indexPath = tableView.indexPathForSelectedRow
-        let contact = contactsList[indexPath?.row ?? 1]
+        let contact = contactsList[section]
         return contact.fullName
     }
 }
